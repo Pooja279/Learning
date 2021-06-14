5b26001fb57ddac95920aded0792eaea1741056b
Pooja279 <pujagyanchandani@gmail.com>
2021-06-14 22:48:41 +0530

    ADd new file test

---
+++ test.js
@@ -0,0 +1,9 @@
+let log= window;//window is global object of client side js
+// alert("hello puja")
+// a =  prompt("eneter your name")
+a = confirm("Are you sure")//it gives true and false
+a = location.toString();
+a = innerHeight
+a =scrollX
+a = history
+console.log(a)
