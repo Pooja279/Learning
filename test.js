9b277851529bafe57cf562d7b422b8f442fa5491
Pooja279 <pujagyanchandani@gmail.com>
2021-06-14 22:50:54 +0530

    Add questionmrk

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 let log= window;//window is global object of client side js
 // alert("hello puja")
 // a =  prompt("eneter your name")
-a = confirm("Are you sure")//it gives true and false
+a = confirm("Are you sure?")//it gives true and false
 a = location.toString();
 a = innerHeight
 a =scrollX
